e43cb986aa64e0df7fa525eda1474c47707a56b4
RodrigoHelu <rodrigoheluani@gmail.com>
2019-09-30 12:22:32 -0300

    #require

--- Contacto.php
+++ Contacto.php
@@ -57,26 +57,7 @@ if ($_POST) {
     <link rel="icon" href="img/favicon.png">
   </head>
   <body>
-    <header>
-    <nav class="navbar navbar-dark bg-primary" id="menunavv">
-      <a class="navbar-brand" href="Home.php" id="navegacion">
-    <img src="img/logo.png" class="d-inline-block align-top img-fluid" alt="Logo OpenFUX" id="imagenlogo">
-      </a>
-        <a class="nav-item nav-link active" href="Home.php" id="textonav">Inicio <span class="sr-only">(current)</span></a>
-        <a class="nav-item nav-link" href="Registro.php" id="textonav">Registrate</a>
-        <a class="nav-item nav-link" href="Perfil.php" id="textonav">Términos y condiciones</a>
-        <ul class="nav">
-          <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" id="barracel">Menú</a>
-          <div class="dropdown-menu dropdown-menu-right">
-          <a class="dropdown-item" href="Home.php">Inicio</a>
-          <a class="dropdown-item" href="Registro.php">Registrate</a>
-          <a class="dropdown-item" href="Perfil.php">Términos y condiciones</a>
-          </div>
-          </li>
-        </ul>
-    </nav>
-  </header>
+<?php require_once("barranav.php"); ?>
   <div class="container">
     <div class="col-12 text-center mt-3"  id="pregfrec"><h2><strong>Contáctanos</strong></h2></div>
       <div class="row">
